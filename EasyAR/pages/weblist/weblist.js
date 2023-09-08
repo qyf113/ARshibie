ea4e29cff5ee146c443fd02644816af94ff5a4ad
qyf113 <59951322+qyf113@users.noreply.github.com>
2023-09-08 14:35:10 +0800

    Add files via upload

---
+++ EasyAR/pages/weblist/weblist.js
@@ -0,0 +1,78 @@
+// pages/weblist.js
+import base64 from "./base64.min";
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    webListDUrl: []
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+    const baselist = JSON.parse(base64.decode(options.id));
+    this.setData({
+      webListDUrl: baselist
+    })
+  },
+  btnurl: function (event) {
+    wx.vibrateShort({
+      type: 'medium'
+    })
+    let urlbase = base64.encode(event.currentTarget.dataset.url);
+    wx.navigateTo({
+      url: '../webUrl/webToUrl?url='+urlbase
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
